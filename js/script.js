7d5a0ccf793d963d7542865a2f8265b1cba4de44
SAI SNEHITH <2303A52006@sru.edu.in>
2025-11-16 22:38:19 +0530

    update

--- js/script.js
+++ js/script.js
@@ -2,13 +2,55 @@
 // PRODUCT DATA
 // ================================
 const products = [
-  { id: 1, title: "Wireless Headphones", price: 2999, img: "https://picsum.photos/id/30/800/600", desc: "Comfortable premium wireless sound." },
-  { id: 2, title: "Smart Watch", price: 4999, img: "https://picsum.photos/id/31/800/600", desc: "Track fitness, calls, and more." },
-  { id: 3, title: "Leather Jacket", price: 5599, img: "https://picsum.photos/id/32/800/600", desc: "Classic premium leather design." },
-  { id: 4, title: "Running Shoes", price: 2499, img: "https://picsum.photos/id/33/800/600", desc: "Lightweight shoes for daily running." },
-  { id: 5, title: "Ceramic Vase", price: 799, img: "https://picsum.photos/id/34/800/600", desc: "Decorative modern ceramic vase." },
-  { id: 6, title: "Bluetooth Speaker", price: 1999, img: "https://picsum.photos/id/35/800/600", desc: "Portable speaker with deep bass." }
+  { 
+    id: 1, 
+    title: "Wireless Headphones", 
+    price: 2999, 
+    img: "https://plus.unsplash.com/premium_photo-1678099940967-73fe30680949?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", 
+    desc: "Comfortable premium wireless sound." 
+  },
+
+  { 
+    id: 2, 
+    title: "Smart Watch", 
+    price: 4999, 
+    img: "https://images.unsplash.com/photo-1575311373937-040b8e1fd5b6?q=80&w=888&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", 
+    desc: "Track fitness, calls, and more." 
+  },
+
+  { 
+    id: 3, 
+    title: "Leather Jacket", 
+    price: 5599, 
+    img: "https://images.unsplash.com/photo-1551028719-00167b16eac5?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", 
+    desc: "Classic premium leather design." 
+  },
+
+  { 
+    id: 4, 
+    title: "Running Shoes", 
+    price: 2499, 
+    img: "https://images.unsplash.com/photo-1542291026-7eec264c27ff?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", 
+    desc: "Lightweight shoes for daily running." 
+  },
+
+  { 
+    id: 5, 
+    title: "Ceramic Vase", 
+    price: 799, 
+    img: "https://plus.unsplash.com/premium_photo-1668620538983-c5993e4a443d?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", 
+    desc: "Decorative modern ceramic vase." 
+  },
+
+  { 
+    id: 6, 
+    title: "Bluetooth Speaker", 
+    price: 1999, 
+    img: "https://images.unsplash.com/photo-1589256469067-ea99122bbdc4?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", 
+    desc: "Portable speaker with deep bass." 
+  }
 ];
+
 
 // ================================
 // DOM ELEMENTS
